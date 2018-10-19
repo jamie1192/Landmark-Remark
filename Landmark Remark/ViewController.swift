675525f02163230df1df08aa31b5238c4d314004
Jamie <jastley92@gmail.com>
2018-10-19 12:57:03 +1100

    Initial Commit

---
+++ Landmark Remark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark Remark
+//
+//  Created by Jamie Astley on 19/10/18.
+//  Copyright © 2018 jastley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
